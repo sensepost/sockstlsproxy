e17f632af69a20d08f3c0e6ed34a96f77c4dd51b
Mamadou Lamine NIANG <mlniangpro@gmail.com>
2023-02-01 13:49:48 +0000

    Added commandlineparser and possibility to automatically generate server certificate

---
+++ Proxy/CertificateUtil.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Proxy
+{
+    internal class CertificateUtil
+    {
+        public static X509Certificate GetServerCert(string serverCertName)
+        {
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+
+            X509Certificate2 certificate = null;
+            foreach (X509Certificate2 currentCertificate in store.Certificates)
+            {
+                if (currentCertificate.IssuerName.Name != null &&
+                    currentCertificate.IssuerName.Name.Equals("CN=" + serverCertName))
+                {
+                    certificate = currentCertificate;
+                    break;
+                }
+            }
+
+            return certificate;
+        }
+
+        public static X509Certificate MakeCert(string certName)
+        {
+            Console.WriteLine($"[*] Creating self-signed certificate {certName}");
+            try
+            {
+                ECDsa ecdsa = ECDsa.Create(); // generate asymmetric key pair
+                CertificateRequest req = new CertificateRequest("cn=" + certName, ecdsa, HashAlgorithmName.SHA256);
+                X509Certificate2 cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
+
+                if(!cert.HasPrivateKey)
+                {
+                    cert = cert.CopyWithPrivateKey(ecdsa);
+                }
+
+                var persistable = new X509Certificate2(cert.Export(X509ContentType.Pfx), "", X509KeyStorageFlags.PersistKeySet);
+
+                using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Add(persistable);
+                }
+
+                Console.WriteLine($"[+] Certificate {certName} created and Added to My store");
+                return persistable;
+            } catch(Exception ex)
+            {
+                Console.WriteLine($"[-] Error creating certificate {certName}");
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+        public static X509Certificate GetOrCreateServerCert(string serverCertName)
+        {
+            X509Certificate cert = GetServerCert(serverCertName);
+            if(cert == null)
+            {
+                Console.WriteLine($"[-] Server cert {serverCertName} not found in my store.");
+                cert = MakeCert(serverCertName);
+            }
+
+            return cert;
+        }
+
+        public static X509Certificate GetClientCert(string name)
+        {
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach (X509Certificate2 currentCertificate in store.Certificates)
+            {
+                if (currentCertificate.IssuerName.Name != null &&
+                    currentCertificate.SubjectName.Name.StartsWith("CN=" + name))
+                {
+                    return currentCertificate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- Proxy/Socks5/Socks5Handler.cs
+++ Proxy/Socks5/Socks5Handler.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using System.Net;
 using System.Threading.Tasks;
 
 namespace Proxy
